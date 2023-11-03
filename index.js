3fac7fcf26c9862aacf5557cb052d07d7a9af173
amandasatya <amandasatya93@gmail.com>
2023-11-03 19:33:53 +0700

    adding darkmode

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
     menuToggle.classList.toggle('active');
 });
+
+const body = document.body;
+const changeModeButton = document.getElementById('change-mode');
+const themeStyle = document.getElementById('darkModeWhiteMode');
+
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+if (isDarkMode) {
+    themeStyle.setAttribute('href', './css/navbar.css');
+    changeModeButton.textContent = 'Day Mode';
+} else {
+    themeStyle.setAttribute('href', './css/whitemode.css');
+    changeModeButton.textContent = 'Dark Mode';
+}
+
+changeModeButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    const isDarkMode = body.classList.contains('dark');
+
+    if (isDarkMode) {
+        themeStyle.setAttribute('href', './css/navbar.css');
+        changeModeButton.textContent = 'Day Mode';
+        localStorage.setItem('darkMode', 'true'); // Save the mode preference
+    } else {
+        themeStyle.setAttribute('href', './css/whitemode.css');
+        changeModeButton.textContent = 'Dark Mode';
+        localStorage.setItem('darkMode', 'false'); // Save the mode preference
+    }
+});
+
+console.log('isDarkMode:', isDarkMode);
